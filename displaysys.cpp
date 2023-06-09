236650b9cb116e5ef3fc6639323b75160ea7b9b8
Sanjana <98782398+vulcan20@users.noreply.github.com>
2023-06-09 18:02:49 -0400

    Done

--- displaysys.cpp
+++ displaysys.cpp
@@ -11,106 +11,152 @@
 
 #include "DisplayMsg/DisplayMsg.h"
 
-#define ATTACH_POINT "display_msg_channel" //change the attach point
+#define ATTACH_POINT "display_msg_channel"
 
+#define ATTACH_POINT_DISPLAY_ALARM_MSG "display_alarm_msg_channel"
+
+FILE *opreq_logfile_fd; //LOG FILE
+
+typedef struct AlarmPlaneIds_s
+{
+	uint32_t ids[2];
+
+} AlarmPlaneIds_t;
+
+static std::vector<AlarmPlaneIds_t> alarmMsgVector;
+
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+pthread_cond_t cond = PTHREAD_COND_INITIALIZER;
+
+uint32_t g_msg_counter = 0;
+
+// signal handler function
+void resetAlarmMsgVector_handler(int signum)
+{
+	pthread_mutex_lock(&mutex);
+	{
+		alarmMsgVector.clear();
+	}
+    pthread_cond_broadcast(&cond); // wake up all waiting threads
+    pthread_mutex_unlock(&mutex);
+}
+
+static void _print_plane_info(const Aircraft_t aircraftInfo)
+{
+	if(opreq_logfile_fd == NULL)
+		{
+		opreq_logfile_fd = fopen("C:\\Users\\user\\ide-7.1-workspace\\displaysys\\src\\opreq_logfile.txt", "a+");
+			if (opreq_logfile_fd == NULL)
+			{
+				printf("Failed to open log file.\n");
+				//exit(EXIT_FAILURE);
+				return;
+			}
+		}
+	printf("DISPLAYSYS: AV ID = 0x%08X update rcvd..\n", (uint32_t)aircraftInfo.id);
+	printf("DISPLAYSYS: Position (x, y, z) = (%lf, %lf, %lf)\n", aircraftInfo.position[0], aircraftInfo.position[1], aircraftInfo.position[2]);
+	printf("DISPLAYSYS: Velocity (x, y, z) = (%lf, %lf, %lf)\n", aircraftInfo.velocity[0], aircraftInfo.velocity[1], aircraftInfo.velocity[2]);
+
+	fprintf(opreq_logfile_fd, "\nAV ID = 0x%08X update rcvd..\n", (uint32_t)aircraftInfo.id);
+		fprintf(opreq_logfile_fd, "Position (x, y, z) = (%lf, %lf, %lf)\n", aircraftInfo.position[0], aircraftInfo.position[1], aircraftInfo.position[2]);
+		fprintf(opreq_logfile_fd, "Velocity (x, y, z) = (%lf, %lf, %lf)\n", aircraftInfo.velocity[0], aircraftInfo.velocity[1], aircraftInfo.velocity[2]);
+
+		fflush(opreq_logfile_fd);
+		fclose(opreq_logfile_fd);
+		opreq_logfile_fd = NULL;
+}
+
+static void _print_route_alarm_present(const uint32_t aircraftId1ForAlarm, const uint32_t aircraftId2ForAlarm, const uint32_t rel_dist_feet)
+{
+	int isFoundInAlarmMsgVectorAtIndex = -1;
+	pthread_mutex_lock(&mutex);
+
+	for(uint32_t i = 0; i < alarmMsgVector.size(); i++)
+	{
+		AlarmPlaneIds_t alarmPlaneIds = alarmMsgVector.at(i);
+
+		if(alarmPlaneIds.ids[0] == aircraftId1ForAlarm)
+		{
+			if(alarmPlaneIds.ids[1] == aircraftId2ForAlarm)
+			{
+				isFoundInAlarmMsgVectorAtIndex = i;
+			}
+		}
+	} // for(int i = 0; i < alarmMsgVector.size(); i++)
+
+	if(isFoundInAlarmMsgVectorAtIndex == -1)
+	{
+		printf("DISPLAYSYS: [%04d ALARM-PRESENT] Constraint violation for ID1 = 0x%08X and ID2 = 0x%08X; RelDistFt = %u\r\n",
+				g_msg_counter,
+				aircraftId1ForAlarm,
+				aircraftId2ForAlarm,
+				rel_dist_feet);
+		g_msg_counter++;
+
+		AlarmPlaneIds_t alarmPlaneIds;
+		alarmPlaneIds.ids[0] = aircraftId1ForAlarm;
+		alarmPlaneIds.ids[1] = aircraftId2ForAlarm;
+		alarmMsgVector.push_back(alarmPlaneIds);
+	}
+
+	pthread_cond_broadcast(&cond); // wake up all waiting threads
+	pthread_mutex_unlock(&mutex);
+}
+
+static void _print_route_alarm_for_future_violation(const uint32_t aircraftId1ForAlarm, const uint32_t aircraftId2ForAlarm, const uint32_t rel_dist_feet)
+{
+	printf("DISPLAYSYS: [%04d ALARM-FUTURE] Potential constraint violation for ID1 = 0x%08X and ID2 = 0x%08X; RetDistFt = %u\r\n",
+			g_msg_counter,
+			aircraftId1ForAlarm,
+			aircraftId2ForAlarm,
+			rel_dist_feet);
+	g_msg_counter++;
+}
 
 /*** Client Side of the code ***/
-int RunDisplayMsgSystem()
+void* RunDisplayMsgSystem(void* arg)
 {
    name_attach_t *attach;
 
-   DisplayMsg_t msg;
+   DisplayInfo_t displayInfo;
    int rcvid;
 
    /* Create a local name (/dev/name/local/...) */
-   if ((attach = name_attach(NULL, ATTACH_POINT, 0)) == NULL) {
-	   return EXIT_FAILURE;
+   if ((attach = name_attach(NULL, ATTACH_POINT, 0)) == NULL)
+   {
+	   return (void*)EXIT_FAILURE;
    }
 
    /* Do your MsgReceive's here now with the chid */
    while (1)
    {
-	   /* Server will block in this call, until a client calls MsgSend to send a message to
-		* this server through the channel named "myname", which is the name that we set for the channel,
-		* i.e., the one that we stored at ATTACH_POINT and used in the name_attach call to create the channel. */
-	   //rcvid = MsgReceive(attach->chid, &msg, sizeof(msg), NULL);
-	   rcvid = MsgReceive(attach->chid, &msg, sizeof(DisplayMsg_t), NULL);
 
-	   /* In the above call, the received message will be stored at msg when the server receives a message.
-		* Moreover, rcvid */
+	   rcvid = MsgReceive(attach->chid, &displayInfo, sizeof(DisplayInfo_t), NULL);
+
 
 	   if (rcvid == -1) {/* Error condition, exit */
 		   break;
 	   }
 
-	   if (rcvid == 0) {/* Pulse received */
-		   switch (msg.hdr.code) {
-		   case _PULSE_CODE_DISCONNECT:
-			   /*
-				* A client disconnected all its connections (called
-				* name_close() for each name_open() of our name) or
-				* terminated
-				*/
-			   ConnectDetach(msg.hdr.scoid);
-			   break;
-		   case _PULSE_CODE_UNBLOCK:
-			   /*
-				* REPLY blocked client wants to unblock (was hit by
-				* a signal or timed out).  It's up to you if you
-				* reply now or later.
-				*/
-			   break;
-		   default:
-			   /*
-				* A pulse sent by one of your processes or a
-				* _PULSE_CODE_COIDDEATH or _PULSE_CODE_THREADDEATH
-				* from the kernel?
-				*/
-			   break;
-		   }
-		   continue;
-	   }
-
-	   /* name_open() sends a connect message, must EOK this */
-	   if (msg.hdr.type == _IO_CONNECT ) {
-		   MsgReply( rcvid, EOK, NULL, 0 );
-		   continue;
-	   }
-
-	   /* Some other QNX IO message was received; reject it */
-	   if (msg.hdr.type > _IO_BASE && msg.hdr.type <= _IO_MAX ) {
-		   MsgError( rcvid, ENOSYS );
-		   continue;
-	   }
-
-	   /* Here are the messages that you will work on, i.e., messages that will have a meaning for
-		* your application. Let's assume that you have one message (e.g., data to be displayed) and several subtypes.
-		* Thus, we first test to check if it is a message we expect. Next, we can have a switch that check
-		* what is the subtype of the message. In your project, for instance, you can have a subtype for each
-		* variable, e.g., (0x01 - speed, 0x02 - temperature, 0x03 - gear, and so on...).
-		* Then, based on the subtype the server is receiving, it would display the information
-		* contained in msg.data in the proper place, e.g., at the proper location in a GUI.
-		* You can use that as well to work on the output your thread should provide.
-		*
-		* In addition, you might have another type of message. For instance, you might have a type of message
-		* that would be used for configuration (e.g., type==0x01 used for configuration and type==0x00 for data).
-		* This can be used to implement the mechanism to change the period of your consumer thread (server).
-		* For instance, let's assume that you implemented this server in a separate thread and, instead of having
-		* a forever loop, you implement the thread as a periodic task as we have seen (this is not necessary in this
-		* case because the server will block when waiting for a message, i.e., it will not be hogging CPU.). Then, the
-		* configuratin message could be used to send the period/frequency in which the client would be sending messages.
-		* Thus, the server could consider that while doing other things. */
-	   if (msg.hdr.type == 0x00)
+	   //if (msg.hdr.type == 0x00)
 	   {
-		  if (msg.hdr.subtype == 0x01)
+		  //if (msg.hdr.subtype == 0x01)
 		  {
 			  /* A message (presumable ours) received, handle */
-			  //printf("Server receive %d \n", msg.data);
-			  printf("Received display message type: %d\n", (int)msg.displayInfo.displayType);
 
+			  printf("\nDISPLAYSYS: Received display message type: %d\n", (int)displayInfo.displayType);
 			  //
 			  // TO DO: Implement how to print for different display types
 			  //
+			  switch(displayInfo.displayType)
+			  {
+			  case DISPLAY_TYPE_AIRCRAFT_INFO:
+				  _print_plane_info(displayInfo.aircraftInfoToReport);
+				  break;
+
+			  default:
+				  break;
+			  } // End of switch(msg.displayInfo.displayType)
 
 		  }
 	   }
@@ -122,17 +168,106 @@ int RunDisplayMsgSystem()
    /* Remove the name from the space */
    name_detach(attach, 0);
 
-   return EXIT_SUCCESS;
+   return (void*)EXIT_SUCCESS;
 }
 
-int main(int argc, char **argv) {
-    int ret;
+void* RunAlarmMsgDisplayThread(void* arg)
+{
+   name_attach_t *attach;
+
+   DisplayInfo_t displayInfo;
+   int rcvid;
+
+   alarmMsgVector.clear();
+
+   /* Create a local name (/dev/name/local/...) */
+   if ((attach = name_attach(NULL, ATTACH_POINT_DISPLAY_ALARM_MSG, 0)) == NULL)
+   {
+	   return (void*)EXIT_FAILURE;
+   }
+
+   /* Do your MsgReceive's here now with the chid */
+   while (1)
+   {
+	   rcvid = MsgReceive(attach->chid, &displayInfo, sizeof(DisplayInfo_t), NULL);
+
+	   /* In the above call, the received message will be stored at msg when the server receives a message.
+		* Moreover, rcvid */
+
+	   if (rcvid == -1) {/* Error condition, exit */
+		   break;
+	   }
+	   //if (msg.hdr.type == 0x00)
+	   {
+		  //if (msg.hdr.subtype == 0x01)
+		  {
+			  // Implement how to print for different display types
+			  //
+			  switch(displayInfo.displayType)
+			  {
+			  case DISPLAY_TYPE_ROUTE_ALARM_PRESENT:
+				  _print_route_alarm_present(displayInfo.aircraftIdsForAlarm[0], displayInfo.aircraftIdsForAlarm[1], displayInfo.rel_dist);
+				  break;
+
+			  case DISPLAY_TYPE_ROUTE_ALARM_FUTURE:
+				  _print_route_alarm_for_future_violation(displayInfo.aircraftIdsForAlarm[0], displayInfo.aircraftIdsForAlarm[1], displayInfo.rel_dist);
+				  break;
+
+			  default:
+				  break;
+			  } // End of switch(msg.displayInfo.displayType)
+
+		  }
+	   }
+
+	   MsgReply(rcvid, EOK, 0, 0);
+
+   } // End of while (KEEP_RUNNING_COMPUTER_SERVER == 1)
+
+   /* Remove the name from the space */
+   name_detach(attach, 0);
+
+   return (void*)EXIT_SUCCESS;
+}
 
 
-//    if (strcmp(argv[1], "-c") == 0) {
-        printf("Running RunDisplayMsgSystem ... \n");
-        ret = RunDisplayMsgSystem();   /* see name_open() for this code */
-//    }
+int main(int argc, char **argv)
+{
+    int ret = 0;
+
+    // install signal handler
+	signal(SIGUSR1, resetAlarmMsgVector_handler);
+
+	//
+	// Have the TIMER fire SIGUSR1 on expiration
+	//
+	struct sigevent event;
+	SIGEV_SIGNAL_INIT (&event, SIGUSR1);
+
+	// create timer
+	timer_t timer;
+	struct itimerspec timer_spec;
+	timer_create(CLOCK_REALTIME, &event, &timer);
+	//timer_create(CLOCK_REALTIME, NULL, &timer);
+	timer_spec.it_interval.tv_sec = 10; // 10-second period,it'll update
+	timer_spec.it_interval.tv_nsec = 0;
+	timer_spec.it_value = timer_spec.it_interval;
+	timer_settime(timer, 0, &timer_spec, NULL);
+
+    printf("Running RunDisplayMsgSystem ... \n");
+
+
+	pthread_t thread_id_for_running_display_system;
+	pthread_create(&thread_id_for_running_display_system, NULL, &RunDisplayMsgSystem, NULL);
+
+	pthread_t thread_id_for_running_alarm_msg_display_system;
+	pthread_create(&thread_id_for_running_alarm_msg_display_system, NULL, &RunAlarmMsgDisplayThread, NULL);
+
+    //
+	// Wait for the threads to finish
+	//
+	pthread_join(thread_id_for_running_display_system, NULL);
+	pthread_join(thread_id_for_running_alarm_msg_display_system, NULL);
 
     return ret;
 }
